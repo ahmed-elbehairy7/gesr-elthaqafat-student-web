8f0bd5deaec39d29243e01e91b4e4e618401ec1a
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-01-30 11:45:51 +0200

    auto keyboard avoiding inputs

--- app/template.tsx
+++ app/template.tsx
@@ -3,6 +3,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Template({
 	children,
@@ -12,6 +13,30 @@ export default function Template({
 	const excludeHeaderAndFooter = ["signup", "login"].includes(
 		usePathname().replace("/", "")
 	);
+
+	useEffect(() => {
+		// focus events don't bubble, must use capture phase
+		document.body.addEventListener(
+			"focus",
+			(event) => {
+				const target = event.target as any;
+				switch (target.tagName) {
+					case "INPUT":
+					case "TEXTAREA":
+					case "SELECT":
+						document.body.classList.add("keyboard");
+				}
+			},
+			true
+		);
+		document.body.addEventListener(
+			"blur",
+			() => {
+				document.body.classList.remove("keyboard");
+			},
+			true
+		);
+	}, []);
 	return (
 		<html lang="en">
 			<body className="w-screen h-screen bg-gradient-to-t from-bright-two to-bright-one m-0">

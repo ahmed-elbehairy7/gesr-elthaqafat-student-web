ff1e201e3d0a95d1757067d296ff05abb608e6c1
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-01-30 12:15:10 +0200

    done header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
 
-const Header = async () => {
-	return <div>Header</div>;
+const Header = () => {
+	const coins = 10; //backend todo get user coins
+	return (
+		<header className="w-full h-32 flex flex-col">
+			<div className="w-full px-14 md:px-32 flex flex-row justify-between items-center">
+				<div className="w-fit -m-6">
+					<Image src={Logo} alt="logo" width={125} />
+				</div>
+				<div>
+					<p className="font-semibold text-lg">{coins} 🪙</p>
+				</div>
+			</div>
+			<div className=" w-full h-0.5 bg-gradient-to-r from-transparent via-gray-600 to-transparent mt-2"></div>
+		</header>
+	);
 };
 
 export default Header;

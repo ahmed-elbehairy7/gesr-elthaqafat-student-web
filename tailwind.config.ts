08f52d66f743044b05f6a431e867579809749a78
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-01-30 09:17:34 +0200

    Initial configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				cairo: ["Cairo", "sans-serif"],
+			},
+			borderRadius: {
+				inputField: "15px",
+			},
+			colors: {
+				primary: "#0d309e",
+				"primary-color": "#0d309e",
+				"secondary-color": "#60148c",
+				"accent-color": "#18cad3",
+				"bright-one": "#f2f2f2",
+				"bright-two": "#ccc",
+				"dark-one": "#040e30",
+				"dark-two": "#1d062a",
+				test: "#34fe41",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;

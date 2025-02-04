6e114e0bcb6a7a932689502097a667b2443a2b6c
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-02-04 14:00:38 +0200

    menubar done

--- components/menuBar.tsx
+++ components/menuBar.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import React, { useState } from "react";
 import menuIcon from "@/assets/menu icon.png";
 import closeIcon from "@/assets/close icon.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const MenuBar = () => {
+const MenuBar = ({ name }: MenuBarProps) => {
 	const [showBar, setShowBar] = useState(false);
 	return (
 		<div className="z-10 md:hidden">
@@ -30,9 +32,44 @@ const MenuBar = () => {
 				className={`${
 					!showBar && "hidden"
 				} fixed right-0 top-0 w-2/3 h-full bg-gradient-to-br from-dark-one to-dark-two`}
-			></div>
+			>
+				<div className="mt-20 flex flex-col">
+					{[
+						{ href: "/dashboard", text: "dashboard" },
+						{ href: "/profile", text: "profile" },
+					].map((v, index) => (
+						<MenuBarLink {...v} key={index} />
+					))}
+				</div>
+			</div>
 		</div>
 	);
+};
+
+function MenuBarLink({ text, href }: MenuBarLinkProps) {
+	const pathname = usePathname();
+
+	return (
+		<Link
+			className={` w-full py-3.5 pl-6 font-medium  ${
+				href === pathname
+					? "bg-accent-color text-dark-two"
+					: "text-bright-one border-b border-bright-one"
+			} `}
+			href={href}
+		>
+			{text}
+		</Link>
+	);
+}
+
+export type MenuBarProps = {
+	name: string;
+};
+
+type MenuBarLinkProps = {
+	text: string;
+	href: string;
 };
 
 export default MenuBar;

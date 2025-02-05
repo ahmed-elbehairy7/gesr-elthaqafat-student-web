601d8923017632f70cb83ebd788611186d1796c5
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-02-05 14:07:47 +0200

    setup routing and deps

---
+++ i18nConfig.ts
@@ -0,0 +1,7 @@
+const i18nConfig = {
+	locales: ["ar", "en", "mw"],
+	defaultLocale: "ar",
+	prefixDefault: true,
+};
+
+export default i18nConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import i18nConfig from "./i18nConfig";
+import { i18nRouter } from "next-i18n-router";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-	return NextResponse.redirect(new URL("/dashboard", request.url));
+	// return NextResponse.redirect(new URL("/dashboard", request.url));
+
+	return i18nRouter(request, i18nConfig);
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: "/",
+	matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
